f1e7db23809b8ae3d0ad5d32ed22c03e6b51f60c
arsalan8252 <mernstack8252@gmail.com>
2022-02-26 13:17:17 +0500

    add

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("this is node")
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+
+const port = 4000;
+
+const app = express();
+
+app.use(cors());
+
+app.listen(port, (request, response)=>{
+    console.log(`The server is runing on port ${port}`);
+});
